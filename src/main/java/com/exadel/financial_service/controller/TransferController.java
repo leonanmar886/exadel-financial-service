79ce895052d99434d01e1614864d3b6cd692b04d
Leonan Marques <leonan.marques886@gmail.com>
2024-08-01 21:09:00 -0300

    feat: security configs have been implemented

---
+++ src/main/java/com/exadel/financial_service/controller/TransferController.java
@@ -0,0 +1,15 @@
+package com.exadel.financial_service.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/financial-service")
+public class TransferController {
+    @GetMapping("/transfer")
+    public ResponseEntity<String> transfer() {
+        return ResponseEntity.ok("Transfer successful");
+    }
+}
